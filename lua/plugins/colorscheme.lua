79a63e82f272c58403bf4f579da479e0bc710287
Tomek <lekstomek602@gmail.com>
2024-06-14 10:30:30 +0200

    added cattpuccin and kanagawa paper

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -46,7 +46,7 @@ return {
     },
     ]]
     --
-
+    --[[
     {
         "folke/tokyonight.nvim",
         lazy = false,
@@ -87,6 +87,63 @@ return {
             on_highlights = function(highlights, colors) end,
             opts = {},
         }),
+    }, ]]
+    --
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        require("catppuccin").setup({
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            background = { -- :h background
+                light = "latte",
+                dark = "mocha",
+            },
+            transparent_background = false, -- disables setting the background color.
+            show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+            term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+            dim_inactive = {
+                enabled = false, -- dims the background color of inactive window
+                shade = "dark",
+                percentage = 0.15, -- percentage of the shade to apply to the inactive window
+            },
+            no_italic = false, -- Force no italic
+            no_bold = false, -- Force no bold
+            no_underline = false, -- Force no underline
+            styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" }, -- Change the style of comments
+                conditionals = { "italic" },
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+                -- miscs = {}, -- Uncomment to turn off hard-coded styles
+            },
+            color_overrides = {},
+            custom_highlights = {},
+            default_integrations = true,
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                notify = false,
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
+                },
+                -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+            },
+        }),
+
+        -- setup must be called before loading
+        vim.cmd.colorscheme("catppuccin"),
     },
     --[[
     {
@@ -142,5 +199,34 @@ return {
         colorscheme = { "nord" },
     },
   ]]
+    --
+    --[[
+ {
+  "sho-87/kanagawa-paper.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+require('kanagawa-paper').setup({
+  undercurl = true,
+  transparent = false,
+  gutter = false,
+  dimInactive = true, -- disabled when transparent
+  terminalColors = true,
+  commentStyle = { italic = true },
+  functionStyle = { italic = false },
+  keywordStyle = { italic = false, bold = false },
+  statementStyle = { italic = false, bold = false },
+  typeStyle = { italic = false },
+  colors = { theme = {}, palette = {} }, -- override default palette and theme colors
+  overrides = function()  -- override highlight groups
+    return {}
+  end,
+})
+
+-- setup must be called before loading
+vim.cmd("colorscheme kanagawa-paper")
+}
+
+ ]]
     --
 }
